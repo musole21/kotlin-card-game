1ccbf04f6a8d2023abb2d669b21052f9bf24e981
Musole Yamboto <yambotomusole@gmail.com>
2021-01-02 11:30:35 +0200

    initial commit

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,17 @@
+class Game {
+
+    fun createCards() {}
+
+    fun shuffleDeck() {}
+
+    fun dealCards() {}
+
+    fun checkWinStatus() {}
+}
+
+class Player {
+//    var name: String
+//    var hand: List<Card>
+
+}
+
